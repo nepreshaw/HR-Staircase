ab46d92f49b27dab856bdeda925741d7cd676594
nickp_000 <nickp_000@NICK>
2022-04-24 15:59:08 -0400

    Add project files.

---
+++ HR Staircase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HR_Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 6;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+
+                for (int x = 0; x < n; x++)
+                {
+                    if (x >= i)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+
+        }
+    }
+}
